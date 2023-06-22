f357079ff3af9578709c982b9c9e30b146fac0c3
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2023-06-22 19:00:25 -0300

    aula13

--- aula13 - exercicio/script.js
+++ aula13 - exercicio/script.js
@@ -1,18 +1,9 @@
-let num = document.getElementById('num')
-let lista = document.getElementById('flista')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
 let valores = []
-let n = Number(num.value)
 
 function adicionar() {
-  if (num.value.length == 0) {
-    window.alert('Valor inválido ou ja encontrado na lista.')
-  } else {
-    lista.innerHTML = `TESTANDO`
+  if (isNumero(num.value) && !inLista(num.value, valores)) {
   }
-}
-
-function finalizar() {
-  if (lista.value.length == 0) {
-    window.alert('Adicione valores antes de finalizar!')
-  } 
 }
