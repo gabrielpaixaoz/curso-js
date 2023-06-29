f4e4fbf46f02863d15c3f1a5d327f2646b1231e0
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2023-06-29 14:22:38 -0300

    2

--- aula13 - exercicio/script.js
+++ aula13 - exercicio/script.js
@@ -2,6 +2,7 @@ let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
+let quadro = document.querySelector('option#quadro')
 
 function isNumero(n) {
   if (Number(n) >= 1 && Number(n) <= 100) {
@@ -21,8 +22,33 @@ function inLista(n, l) {
 
 function adicionar() {
   if (isNumero(num.value) && !inLista(num.value, valores)) {
-    window.alert('Número adicionado à lista com sucesso.')
+    valores.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} adicionado.`
+    lista.appendChild(item)
   } else {
     window.alert('Valor inválido ou já encontrado na lista.')
+  }
+}
+
+function finalizar() {
+  if (valores.length == 0) {
+    window.alert('Adicione valores antes de finalizar!')
+  } else {
+    let tot = valores.length
+    const max = Math.max(...valores)
+    const min = Math.min(...valores)
+    const sum = valores.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    )
+    const media = sum / valores.length
+
+    res.innerHTML = ''
+    res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados.</p>`
+    res.innerHTML += `<p> O maior valor infomado foi ${max}.</p> `
+    res.innerHTML += `<p> O menor valor infomado foi ${min}.</p>`
+    res.innerHTML += `<p> Somando todos os valores, temos ${sum}</p>`
+    res.innerHTML += `<p> A média dos valores digitados é ${media}.`
   }
 }
